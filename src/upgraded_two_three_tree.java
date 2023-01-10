b09102360e754b85e4f72f11c3e36b67ae75cce5
kfir7755 <kfir.eliyahu@campus.technion.ac.il>
2023-01-10 22:06:41 +0200

    needs debug

--- src/upgraded_two_three_tree.java
+++ src/upgraded_two_three_tree.java
@@ -1,57 +1,65 @@
-public class upgraded_two_three_tree<K extends Comparable<K>,V> extends two_three_tree<K,V>{
+public class upgraded_two_three_tree<K extends Comparable<K>,V> extends two_three_tree<K,V> {
 
-    private Node<K,V> right_sentinel;
+    private Node<K, V> right_sentinel;
 
     public upgraded_two_three_tree(K max, K min) {
         super(max, min);
     }
 
     @Override
-    public void two_three_init(){
+    public void two_three_init() {
         super.two_three_init();
         this.right_sentinel = this.getRoot().getMiddle();
     }
 
-    private void set_leaves_predecessors(Node<K,V> l,Node<K,V> m,Node<K,V> r){
+    private void set_leaves_predecessors(Node<K, V> l, Node<K, V> m, Node<K, V> r) {
         if (m.isLeaf()) {
-            l.setPredecessor(null);
             m.setPredecessor(null);
             if (r != null) {
                 r.setPredecessor(m);
-                if (r.getKey().compareTo(max) < 0) {
-                    Node<K, V> r_successor = two_three_successor(r);
-                    if (r_successor != null) {
-                        r_successor.setPredecessor(r);
-                    }
-                }
-                m.setPredecessor(l);
+//                if (r.getKey().compareTo(max) < 0) {
+//                    Node<K, V> r_successor = two_three_successor(r);
+//                    if (r_successor != null) {
+//                        r_successor.setPredecessor(r);
+//                    }
+//                }
             } else {
-                m.setPredecessor(l);
-                if (m.getKey().compareTo(max) < 0) {
-                    Node<K, V> m_successor = two_three_successor(m);
-                    if (m_successor != null) {
-                        m_successor.setPredecessor(m);
-                    }
-                }
+//                if (m.getKey().compareTo(max) < 0) {
+//                    Node<K, V> m_successor = two_three_successor(m);
+//                    if (m_successor != null) {
+//                        m_successor.setPredecessor(m);
+//                    }
+//                }
             }
-            if (l.getKey().compareTo(min) > 0) {
-                Node<K, V> l_predecessor = two_three_predecessor(l);
-                if (l_predecessor != null) {
-                    l.setPredecessor(l_predecessor);
-                }
-            }
+            m.setPredecessor(l);
+//            if (l.getKey().compareTo(min) > 0) {
+//                Node<K, V> l_predecessor = two_three_predecessor(l);
+//                if (l_predecessor != null) {
+//                    l.setPredecessor(l_predecessor);
+//                }
+//            }
         }
     }
 
     @Override
     protected void set_children(Node<K, V> x, Node<K, V> l, Node<K, V> m, Node<K, V> r) {
         super.set_children(x, l, m, r);
-        set_leaves_predecessors(l,m,r);
+        set_leaves_predecessors(l, m, r);
     }
 
     public Node<K, V> Max() {
-        Node<K,V> p = right_sentinel.getP();
-       if (p.getRight()!=null) return p.getMiddle();
-       return p.getLeft();
+        Node<K, V> p = right_sentinel.getP();
+        if (p.getRight() != null) return p.getMiddle();
+        return p.getLeft();
+    }
+
+    protected void insert(Node<K, V> z) {
+        K key = z.getKey();
+        super.insert(z);
+        z = two_three_search(this.getRoot(), key);
+        Node<K, V> z_successor = two_three_successor(z);
+        if (z_successor != null) z_successor.setPredecessor(z);
+        Node<K, V> z_predecessor = two_three_predecessor(z);
+        if (z_predecessor != null) z.setPredecessor(z_predecessor);
     }
 }
